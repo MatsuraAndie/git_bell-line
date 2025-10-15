4cf01e81298447b5cd76bb1f983b0a1095add607
DESKTOP-8AD3JKT\User <matsuura@and-ie.com>
2025-10-15 14:26:37 +0900

    20251015_松浦_メニュー画面追加

--- bell-line/resources/views/layouts/app.blade.php
+++ bell-line/resources/views/layouts/app.blade.php
@@ -1,13 +1,17 @@
 <!doctype html>
 <html lang="ja">
+
 <head>
   <meta charset="utf-8">
   <title>ベルライン システム</title>
+
   @vite(['resources/css/app.css', 'resources/js/app.js'])
   @stack('styles')
 </head>
+
 <body class="bg-gray-100">
-  <header class="bg-gray-200 p-4 font-bold">ベルライン　システムメニュー</header>
+  <header class="bg-blue-500 text-white p-4 font-bold shadow-md">ベルライン　システムメニュー</header>
   <main class="p-6">@yield('content')</main>
 </body>
+
 </html>
